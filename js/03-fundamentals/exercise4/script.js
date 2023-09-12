493bdf2d734d0893364977fb6befa791aef6cce7
josealvarezz <jalvchi273@g.educaand.es>
2023-09-12 14:05:58 +0200

    Exercise 4 finished

---
+++ js/03-fundamentals/exercise4/script.js
@@ -0,0 +1,6 @@
+function lastLetter(String) {
+    const lastLetter = String.substring(String.length, String.length - 1);
+    return lastLetter;
+}
+
+console.log(lastLetter('For this to work it should show the letter i'));
